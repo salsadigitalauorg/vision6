2d51d25ed24d10a9b2937d05b30b968c4674d82f
zulkipli <azulkipli@gmail.com>
2016-03-01 14:03:24 +0700

    Update some doc comments, true false etc

--- js/vision6.js
+++ js/vision6.js
@@ -1,11 +1,15 @@
 /**
+ * @file Vision6 .js file.
+ */
+
+/**
  * Javascript for donate entity module.
  */
 
 (function ($, Drupal, window, document, undefined) {
     $(document).ready(function(){
         $('#edit-field-vis6-new-published-as-synd-und').on('change', function() {
-            $('input[name="field_vis6_published_as_synd[und]"]').prop('checked', $(this).prop( "checked"));
+            $('input[name="field_vis6_published_as_synd[und]"]').prop('checked', $(this).prop("checked"));
         });
         $('#edit-field-vis6-new-syndication-type-und').on('change', function() {
             $('select[name="field_vis6_syndication_type[und]"]').val($(this).val());
@@ -20,7 +24,7 @@
             $('select[name="field_vis6_contact_list_id[und]"]').val($(this).val());
         });
         $('#edit-field-vis6-new-inc-body-synd-und').on('change', function() {
-            $('input[name="field_vis6_inc_body_synd[und]"]').prop('checked', $(this).prop( "checked"));
+            $('input[name="field_vis6_inc_body_synd[und]"]').prop('checked', $(this).prop("checked"));
         });
     });
 })(jQuery, Drupal, this, this.document);
